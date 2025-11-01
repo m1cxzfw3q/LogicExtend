51d2c3f869add8cd2cc50a4a95feea3882e77ba4
m1cxzfw3q <mcxzfwq@outlook.com>
2025-11-01 16:48:17 +0800

    update

--- src/logicExtend/LStringMerge.java
+++ src/logicExtend/LStringMerge.java
@@ -1,7 +1,5 @@
 package logicExtend;
 
-import arc.func.Func;
-import arc.func.Prov;
 import arc.scene.ui.layout.Table;
 import mindustry.gen.LogicIO;
 import mindustry.logic.*;
@@ -32,6 +30,11 @@ public class LStringMerge {
         public LCategory category() {
             return LCategory.operation;
         }
+
+        public static void create() {
+            LAssembler.customParsers.put("stringmerge", params -> new StringMergeStatement());//ai写的史，能跑就行
+            LogicIO.allStatements.add(StringMergeStatement::new);
+        }
     }
 
     public static class LStringMergeI implements LExecutor.LInstruction {
@@ -49,10 +52,5 @@ public class LStringMerge {
         public void run(LExecutor exec) {
             output.setobj(p1.obj().toString() + p2.obj().toString());
         }
-    }
-
-    public static void load() {
-        LAssembler.customParsers.put("stringmerge", params -> new StringMergeStatement());//ai写的史，能跑就行
-        LogicIO.allStatements.add(StringMergeStatement::new);
     }
 }
