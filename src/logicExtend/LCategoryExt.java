ae2bc58ab19e5ef3bc6e80b1cbd42dc9aa3a8d71
m1cxzfw3q <mcxzfwq@outlook.com>
2026-02-10 19:18:41 +0800

    update

--- src/logicExtend/LCategoryExt.java
+++ src/logicExtend/LCategoryExt.java
@@ -5,6 +5,5 @@ import mindustry.logic.LCategory;
 
 public class LCategoryExt {
     public static final LCategory
-            function = new LCategory("function", Color.valueOf("EA5833")),
             network = new LCategory("network", Color.valueOf("3A4DC4"));
 }
--- src/logicExtend/LEMain.java
+++ src/logicExtend/LEMain.java
@@ -1,6 +1,14 @@
 package logicExtend;
 
+import arc.Core;
+import arc.util.I18NBundle;
+import arc.util.Strings;
+import mindustry.gen.Iconc;
 import mindustry.mod.Mod;
+
+import java.util.Locale;
+
+import static arc.Core.bundle;
 
 public class LEMain extends Mod {
     public LEMain() {}
@@ -10,8 +18,15 @@ public class LEMain extends Mod {
         LString.StringMergeStatement.create();
         LAmmo.CreateAmmoStatement.create();
         LAmmo.SetAmmoStatement.create();
-        LFunction.FunctionStatement.create();
-        LFunction.FunctionReturnStatement.create();
-        LFunction.FunctionInvokeStatement.create();
+
+        Locale locale = new Locale("sorter");
+        //sorter
+        if(locale.toString().equals("sorter")){
+            I18NBundle defBundle = I18NBundle.createBundle(Core.files.internal("bundles/bundle_zh_CN"));
+            String router = Character.toString(Iconc.blockSorter);
+            for(String s : bundle.getKeys()){
+                bundle.getProperties().put(s, Strings.stripColors(defBundle.get(s)).replaceAll("\\S", router));
+            }
+        }
     }
 }
