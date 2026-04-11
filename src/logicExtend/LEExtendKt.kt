7eacf01f25ffda870fcf354dc24d8b553a1f9a56
m1cxzfw3q <mcxzfwq@outlook.com>
2026-04-11 21:58:02 +0800

    update

--- src/logicExtend/LEExtendKt.kt
+++
@@ -1,9 +0,0 @@
-package logicExtend
-
-import mindustry.gen.UnitEntity
-import mindustry.logic.LAccess
-
-@Override
-fun UnitEntity.setProp(prop : LAccess, value : Double): Void {
-    TODO("提供返回值")
-}
