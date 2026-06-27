0fb784a4a1a5f09c648bab6b8fde72f6c41b9d91
m1cxzfw3q <mcxzfwq@outlook.com>
2026-06-27 19:47:26 +0800

    update

--- src/logicExtend/LEMain.java
+++ src/logicExtend/LEMain.java
@@ -1,6 +1,8 @@
 package logicExtend;
 
 import arc.Events;
+import arc.util.Log;
+import arc.util.Reflect;
 import logicExtend.dialog.SoundSelector;
 import mindustry.Vars;
 import mindustry.game.EventType;
@@ -25,15 +27,21 @@ public class LEMain extends Mod {
         //LNetwork.load();
         LContentPatchOp.PatchOpStatement.create();
 
-        Events.on(EventType.ClientLoadEvent.class, e -> {
-            Vars.ui.logic = new LELogicDialog();
-            effects = new EffectsDialog(getEffectList());
-            sound = new SoundSelector();
-        });
-
         try {
             Class.forName("mindustryX.VarsX", true, Vars.mods.mainLoader());
             mdtXMode = true;
         } catch (Exception ignored) {}
+
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            Vars.ui.logic = new LELogicDialog();
+            effects = new EffectsDialog(getEffectList());
+            sound = new SoundSelector();
+            try {
+                if (mdtXMode) Reflect.invoke(Class.forName("mindustryX.features.ui.LogicSupport", true, Vars.mods.mainLoader()), "init");
+            } catch (ClassNotFoundException ignored) {
+                mdtXMode = false;
+                Log.warn("[LogicExtend] MDTX 模式已关闭");
+            }
+        });
     }
 }
